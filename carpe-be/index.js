14701410792968d552edd8e4a8bdf9123f804c21
Works Better <worksbetter12@gmail.com>
2022-06-23 22:32:09 +0500

    Add base components folder

--- carpe-be/index.js
+++ carpe-be/index.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 
 const app = new express();
-app.use(bodyParser.json({limit:"50mb", extended:true}));
-app.use(bodyParser.urlencoded({limit:"50mb", extended:true}));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
-dotenv.config({path: "../properties/.env"});
+dotenv.config({ path: "../properties/.env" });
 
 const port = process.env.port;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser : true , useUnifiedTopology : true})
-.then(()=>app.listen(port, ()=>console.log(`server running on port: ${port}` )))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(port, () => console.log(`server running on port: ${port}`))
+  );

37d1c335b14a2604adcb3a4dc9d1e7f9471e461b
mrizwan18 <razibhatti1@gmail.com>
2024-10-15 22:12:25 +0100

    backend with spring

---
+++ carpe-be/carpe/src/main/java/com/zabarzer/carpe/CarpeApplication.java
@@ -0,0 +1,13 @@
+package com.zabarzer.carpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpeApplication.class, args);
+	}
+
+}

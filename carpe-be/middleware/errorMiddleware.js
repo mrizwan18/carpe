3d0879d73ba8a13cc711960a0bc5624b3e8e88fc
mrizwan18 <razibhatti1@gmail.com>
2024-10-15 22:12:45 +0100

    backend with spring

--- carpe-be/middleware/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode ? err.statusCode : 500;
-
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "prod" ? null : err.stack,
-  });
-};

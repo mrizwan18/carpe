5296a6b1e568e106db5d4b2f3579c839d635d8b7
mrizwan18 <razibhatti1@gmail.com>
2024-10-25 00:53:13 +0100

    oauth2 and login

--- carpe-be/carpe/src/main/java/com/zabarzer/carpe/config/SecurityConfig.java
+++ carpe-be/carpe/src/main/java/com/zabarzer/carpe/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,7 +23,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated() // All other requests require authentication
                 )
                 .oauth2Client(withDefaults())  // Configure OAuth2 client
-                .oauth2ResourceServer(withDefaults());  // Configure OAuth2 resource server
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
+                        .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                        .decoder(jwtDecoder())
+                ));
 
         return http.build();
     }
@@ -29,5 +34,9 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://www.googleapis.com/oauth2/v3/certs").build();
     }
 }
